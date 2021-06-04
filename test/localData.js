d67d1afc37f5998058e878fe7cc2feb214f6e334
arahovhannisyan-endigital <85372854+arahovhannisyan-endigital@users.noreply.github.com>
2021-06-04 23:18:51 +0100

    Add files via upload

---
+++ test/localData.js
@@ -0,0 +1,12 @@
+var localData = [
+  {
+    "id": "b950f77d-7031-4670-badd-9495cefa3ebd",
+    "completed": false,
+    "title": "My task"
+  },
+  {
+    "id": "d8be8ea1-9745-4c65-a4cb-9fde1a518b0c",
+    "completed": true,
+    "title": "Pay Bills"
+  }
+];
